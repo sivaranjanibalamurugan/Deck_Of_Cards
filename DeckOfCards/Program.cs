fdab75f3aacac54945de0eee70bc9b689313a103
unknown <sivaranjanib99@gmail.com>
2021-07-07 13:21:35 +0530

    [siva] creating a deck of cards

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Deck of cards!!");
+        }
+    }
+}
